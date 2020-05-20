de2c6259905bce1f522664c58d8ca58ec984e8ba
Khelil Sator <khelil.sator.git@gmail.com>
2020-05-21 00:29:06 +0200

    Update print_server_capabilities.py

--- print_server_capabilities.py
+++ print_server_capabilities.py
@@ -7,6 +7,4 @@ eos=manager.connect(host="10.83.28.203", port="830", timeout=30, username="arist
 for item in eos.server_capabilities: 
     print (item)
 
-assert("urn:ietf:params:xml:ns:yang:ietf-netconf-monitoring" in eos.server_capabilities), "NetConf server not compliant with https://tools.ietf.org/html/rfc6022"
-
 eos.close_session()
