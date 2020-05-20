479ceb3fab4e6a04231b3ceba8d5f42e63b53df6
Khelil Sator <khelil.sator.git@gmail.com>
2020-05-20 22:02:26 +0200

    Update client_capabilities.py

--- client_capabilities.py
+++ client_capabilities.py
@@ -1,7 +1,10 @@
 from ncclient import manager
+
 eos=manager.connect(host="10.83.28.203", port="830", timeout=30, username="arista", password="arista", hostkey_verify=False)
+
 for item in eos.client_capabilities: 
     print (item)
+
 eos.close_session()
 
 
