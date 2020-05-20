9939e55ed8e8bf325b38c3bb7727a4d4cf74bbab
Khelil Sator <khelil.sator.git@gmail.com>
2020-05-21 01:55:11 +0200

    Update edit_config.py

--- edit_config.py
+++ edit_config.py
@@ -8,32 +8,45 @@ conf = '''
             <domain-name>abc.xyz</domain-name>
             <hostname>switch1</hostname>
         </config>
-            <dns>
-                <config>
-                    <network-instance xmlns="http://arista.com/yang/openconfig/system/augments">default</network-instance>
-                </config>
-                    <servers>
-                        <server>
+        <dns>
+            <config>
+                <network-instance xmlns="http://arista.com/yang/openconfig/system/augments">default</network-instance>
+            </config>
+                <servers>
+                    <server>
+                        <address>8.8.8.8</address>
+                        <config>
                             <address>8.8.8.8</address>
-                            <config>
-                                <address>8.8.8.8</address>
-                                <port>53</port>
-                            </config>
-                        </server>
-                        <server>
+                            <port>53</port>
+                         </config>
+                    </server>
+                    <server>
+                        <address>10.83.28.52</address>
+                        <config>
                             <address>10.83.28.52</address>
-                            <config>
-                                <address>10.83.28.52</address>
-                                <port>53</port>
-                            </config>
-                        </server>
-                    </servers>
-            </dns>
+                            <port>53</port>
+                        </config>
+                    </server>
+                </servers>
+        </dns>
     </system>
 </config>
 '''
 
 configuration = eos.edit_config(target = "running", config = conf)
-print (configuration) 
+print(configuration)
+
+
+dns_servers='''
+<system>
+    <dns>
+        <servers>
+        </servers>
+    </dns>
+</system>
+'''
+
+dns_servers_conf = eos.get_config(source="running", filter=("subtree", dns_servers))
+print (dns_servers_conf)
 
 eos.close_session()
