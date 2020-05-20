050621ea1777ee4a5cab66e0673a33a622764e8d
Khelil Sator <khelil.sator.git@gmail.com>
2020-05-20 22:04:44 +0200

    Create server_capabilities.py

---
+++ server_capabilities.py
@@ -0,0 +1,10 @@
+from ncclient import manager
+
+eos=manager.connect(host="10.83.28.203", port="830", timeout=30, username="arista", password="arista", hostkey_verify=False)
+
+for item in eos.server_capabilities: 
+    print (item)
+
+assert("urn:ietf:params:xml:ns:yang:ietf-netconf-monitoring" in eos.server_capabilities), "NetConf server not compliant with https://tools.ietf.org/html/rfc6022"
+
+eos.close_session()
