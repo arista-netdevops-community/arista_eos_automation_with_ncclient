c26fb1cd2b37c69585912021cfab16a093e077a2
Khelil Sator <khelil.sator.git@gmail.com>
2020-05-21 00:21:54 +0200

    Update and rename server_capabilities.py to print_server_capabilities.py

--- print_server_capabilities.py
+++ print_server_capabilities.py
@@ -1,3 +1,5 @@
+# print server capabilities 
+
 from ncclient import manager
 
 eos=manager.connect(host="10.83.28.203", port="830", timeout=30, username="arista", password="arista", hostkey_verify=False)
